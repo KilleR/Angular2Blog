1e16ee3615480732d78eacbbe33c198ba48d9ca7
Richard Kille <richard.kille@juniperbridge.com>
2017-09-12 10:42:53 +0100

    Added routing .htaccess

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Post} from "../post";
-import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-dashboard',
@@ -9,68 +7,9 @@ import {Http} from "@angular/http";
 })
 export class DashboardComponent implements OnInit {
 
-    private blogPosts: Post[];
-    private newPost: Post;
-    private loggedIn: boolean;
-    private loginField: string;
-    private loadingBarMode: string;
-    private loadingBarShow: boolean;
-
-    constructor(private http:Http) {
-        this.newPost = new Post();
-        this.loggedIn = false;
-        this.loadingBarShow = false;
-        this.loadingBarMode = 'indeterminate';
-    }
-
-
-    title = 'Angular 2 Dev Blog!';
+    constructor() { }
 
     ngOnInit() {
-        this.getData();
-    }
-
-    getData() {
-        this.loadingBarMode = 'indeterminate';
-        this.loadingBarShow = true;
-        this.http.get('assets/fileService.php')
-            .subscribe(res => {
-                this.loadingBarMode = 'query';
-                this.parsePosts(res.json());
-                this.loadingBarShow = false;
-            })
-    }
-
-    parsePosts(posts) {
-        this.blogPosts = posts;
-        // for(let i=0; i<this.blogPosts.length; i++) {
-        //     this.blogPosts[i].content = this.blogPosts[i].content.replace(new RegExp('\n', 'g'), "<br />")
-        // }
-    }
-
-    setData() {
-        this.http.post('assets/fileService.php', [{name: "fish"},{name: "bors"}])
-            .subscribe(res => this.blogPosts = res.json())
-    }
-
-    addPost() {
-        this.http.post('assets/fileService.php', this.newPost)
-            .subscribe(res => {
-                this.blogPosts = res.json();
-                this.newPost = new Post();
-            })
-    }
-
-    doLogin(event) {
-        this.loginField = event;
-        this.http.post('assets/loginService.php', {loginField: this.loginField})
-            .subscribe(res => this.checkLogin(res.json()))
-    }
-    checkLogin(res) {
-        this.loginField = '';
-        if(typeof res.loggedIn != "undefined") {
-            this.loggedIn = true;
-        }
     }
 
 }
